1a107372448ef173c8fc071410f604a8a25db3f5
Matthew Deloughry <matt@deloughry.com>
2018-01-21 21:33:41 +0000

    tweaks

--- options.js
+++ options.js
@@ -1,3 +1,4 @@
+function escapeHTML(str) { return str.replace(/[&"'<>]/g, (m) => ({ "&": "&amp;", '"': "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;" })[m]); }
 
 var url = 'https://api.coinmarketcap.com/v1/ticker/unitus/';
 var xhr = new XMLHttpRequest();
@@ -7,12 +8,12 @@ xhr.onload = function() {
   var status = xhr.status;
   if (status === 200) {
     var response = xhr.response[0];
-    document.getElementById('btcprice').innerHTML = Sanitizer.unwrapSafeHTML(response.price_btc + ' BTC');
-    document.getElementById('usdprice').innerHTML = Sanitizer.unwrapSafeHTML(response.price_usd + ' USD');
-    document.getElementById('marketcap').innerHTML = Sanitizer.unwrapSafeHTML(response.market_cap_usd + ' USD');
-    document.getElementById('1percentage').innerHTML = Sanitizer.unwrapSafeHTML(response.percent_change_1h + '%');
-    document.getElementById('24percentage').innerHTML = Sanitizer.unwrapSafeHTML(response.percent_change_24h + '%');
-    document.getElementById('7dpercentage').innerHTML = Sanitizer.unwrapSafeHTML(response.percent_change_7d + '%');
+    document.getElementById('btcprice').innerHTML = escapeHTML(response.price_btc + ' BTC');
+    document.getElementById('usdprice').innerHTML = escapeHTML(response.price_usd + ' USD');
+    document.getElementById('marketcap').innerHTML = escapeHTML(response.market_cap_usd + ' USD');
+    document.getElementById('1percentage').innerHTML = escapeHTML(response.percent_change_1h + '%');
+    document.getElementById('24percentage').innerHTML = escapeHTML(response.percent_change_24h + '%');
+    document.getElementById('7dpercentage').innerHTML = escapeHTML(response.percent_change_7d + '%');
     
   } else {
     console.log (response);
