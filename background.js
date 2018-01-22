5529bf0fa12589b5e358fd7e4d4d9ef1c6d479c1
Matthew Deloughry <matt@deloughry.com>
2018-01-22 08:38:24 +0000

    Changed Homepage url

--- background.js
+++ background.js
@@ -7,25 +7,20 @@ function getJSON(callback) {
     var status = xhr.status;
     if (status === 200) {
       var response = xhr.response;
-      browser.browserAction.setBadgeBackgroundColor({color: "green"});
       chrome.browserAction.setBadgeBackgroundColor({color: "green"});
-      browser.browserAction.setBadgeText({text: response[0].price_usd});
       chrome.browserAction.setBadgeText({text: response[0].price_usd});
-      browser.browserAction.setTitle({title: "1 UIS = " +response[0].price_usd + " USD"});
+      chrome.browserAction.setTitle({title: "1 UIS = " +response[0].price_usd + " USD"});
     } else {
-      browser.browserAction.setBadgeText({text: 'NaN'});
       chrome.browserAction.setBadgeText({text: 'NaN'});
-      browser.browserAction.setBadgeBackgroundColor({color: "red"});
       chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-      
-      browser.browserAction.setTitle({title: response[0].price_usd});
-      
+      chrome.browserAction.setTitle({title: response[0].price_usd});
+
     }
   };
   xhr.send();
 };
 
 
-setInterval(function() { 
+setInterval(function() {
   getJSON();
 }, 6000);
